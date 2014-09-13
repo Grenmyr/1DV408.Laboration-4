4283e9e97ae1f672947f36832626dc713e39b2fa
Grenmyr <dg222cs@student.lnu.se>
2014-09-13 16:33:27 +0200

    Klar med UC 1 & 2. Förutom Head view (får fixa det eventuellt). Har påbörjat sätta upp en cookie class.

---
+++ src/model/UserModel.php
@@ -0,0 +1,42 @@
+<?php
+require_once("./src/model/SessionModel.php");
+
+class UserModel{
+    private $username = "Admin";
+    private $password = "Password";
+    private $sessionModel;
+
+    public function __construct() {
+        // Todo: Här i eller annan model klass ska jag kontrollera att input username och password från klient är
+        // Giltig data innan jag gör strängjämförelsen med min databas uppgift.
+        $this->sessionModel = new SessionModel();
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * Sets my authenticated variable to true if correct username and password from LoginView.
+     */
+    public function logIn( $username, $password){
+        //var_dump($this->username == $username && $this->password == $password);
+        if ($this->username == $username && $this->password == $password) {
+
+            $this->sessionModel->SetValidSession();
+            return true;
+        }
+        return false;
+    }
+    public function LogOut(){
+        $this->sessionModel->UnsetSession();
+    }
+
+    public function IsAuthenticated(){
+        return $this->sessionModel->CheckValidSession();
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: dav
+ * Date: 2014-09-09
+ * Time: 13:50
+ */ 
