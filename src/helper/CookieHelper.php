be657fd71ff7b4038b0d9e6ca36b9b2cc6670fd4
Grenmyr <dg222cs@student.lnu.se>
2014-09-13 17:10:10 +0200

    Förslöer fortfarande få upp en fil, vet ej vad felet är

---
+++ src/helper/CookieHelper.php
@@ -0,0 +1,29 @@
+<?php
+class Cookie {
+    private static $cookieName = "CookieStorage";
+    public function save($string) {
+        setcookie( self::$cookieName, $string, -1);
+        //var_dump($_COOKIE);
+    }
+    public function load() {
+//$ret = isset($_COOKIE["CookieStorage"]) ? $_COOKIE["CookieStorage"] : "";
+        if (isset($_COOKIE[self::$cookieName]))
+            $ret = $_COOKIE[self::$cookieName];
+        else
+            $ret = "";
+        setcookie(self::$cookieName, "", time() -1);
+        return $ret;
+    }
+}
+/**
+ * Created by PhpStorm..
+ * User: dav
+ * Date: 2014-09-13
+ * Time: 16:08
+ */
+/**
+ * Created by PhpStorm.
+ * User: dav
+ * Date: 2014-09-13
+ * Time: 17:05
+ */ 
